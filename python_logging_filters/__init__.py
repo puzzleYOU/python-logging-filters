fe67815f259fdbf19e6db1d07cff2839728dbc50
Florian Schieder <florian.schieder@puzzleyou.de>
2024-10-28 09:04:57 +0100

    enhanced documentation

--- python_logging_filters/__init__.py
+++ python_logging_filters/__init__.py
@@ -5,8 +5,30 @@ import re
 class DjangoHttp404LogFilter(logging.Filter):
     """Suppresses Django's default 'Not Found: ...' log messages.
 
-    See the Django docs how to attach filters to logging handlers via
-    the LOGGING setting.
+    This filter can be integrated into the django `LOGGING` setting,
+    see the following excerpt as an example:
+
+    LOGGING = {
+        ...,
+        "handlers": {
+            "console": {
+                ...,
+                "filters": ["http404"],
+            },
+        },
+        "filters": {
+            "http404": {
+                "()": "python_logging_filters.DjangoHttp404LogFilter",
+            }
+        },
+        ...,
+    }
+
+    NOTE: At the moment, this filter does only take the desired effect with
+    production settings and using a mature WSGI-compliant server (not
+    `./manage.py runserver`). Using the Django debug server or setting
+    `DEBUG = True` issues a "Not Found" log warning which cannot be trivially
+    filtered.
     """
     PATTERN = re.compile("^Not Found:")
 
--- setup.py
+++ setup.py
@@ -1,28 +1,11 @@
-from textwrap import dedent
-
 from setuptools import find_packages, setup
 
 setup(
     name="python-logging-filters",
     version="0.1.2",
     description="standard python logging filters",
-    long_description=dedent(
-        """
-        Simple filters for standard python logging, e.g. for suppressing
-        noisy 3rd party framework logging.
-
-        Current implementations
-        =======================
-
-        DjangoHttp404LogFilter
-        ----------------------
-
-        Suppresses Django's default 'Not Found: ...' logging.
-        See the `python_logging_filters.DjangoHttp404LogFilter`
-        and the official Django documentation for more details how
-        to use and configure that filter.
-        """
-    ),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="puzzleYOU GmbH",
     author_email="scrum@puzzleyou.de",
     url="https://github.com/puzzleYOU/python-logging-filters/",
